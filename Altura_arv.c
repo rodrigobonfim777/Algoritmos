7417b99c0eef15d4a9c555d471df1dfa2d2d3ce7
Rodrigo Bonfim <rodrigobonfa9@gmail.com>
2026-07-22 17:09:14 -0300

    comentários

--- Altura_arv.c
+++ Altura_arv.c
@@ -11,12 +11,16 @@ typedef Tarv *Parv;
 int Altura_arv(Parv a){
     int he, hd;
 
+    // ÁRVORE ESTÁ VAZIA
     if(a == NULL){
         return -1;
     }
+    
+    // GUARDANDO O RESULTADO EM VARIAVEIS ESQ OU DIR
     he = Altura_arv(a -> esq);
     hd = Altura_arv(a -> dir);
 
+    // SEGUINDO O CAMINHO COM RECURSÃO ATÉ CHEGAR EM NULL
     if(he > hd){
         return 1 + he;
     }
