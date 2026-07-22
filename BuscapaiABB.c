f3eb8a9a25df16942738c0bcacff1764e8f23d15
Rodrigo Bonfim <rodrigobonfa9@gmail.com>
2026-07-22 16:20:54 -0300

    Comentários

--- BuscapaiABB.c
+++ BuscapaiABB.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+// ESTRUTURA DA ÁRVORE
 typedef struct arv{
     int info;
     struct arv *esq, *dir;
@@ -8,12 +9,18 @@ typedef struct arv{
 typedef Tarv *Parv;
 
 Parv BuscapaiABB(Parv a, int x){
+    
+    // SE A ÁRVORE ESTIVER VAZIA
     if(a == NULL){
         return NULL;
     }
+
+    // SE O X ESTIVER E EXISTER A ESQ OU A DIR DA RAÍZ
     if(a -> esq != NULL && a -> esq -> info == x || a -> dir != NULL && a -> dir -> info == x){
         return a;
     }
+
+    // CHAMANDO A RECURSÃO COM A PROPRIEDADE DA ABB. ESQ < RAÍZ < DIR
     if(x < a -> info){
         return BuscapaiABB(a -> esq, x);
     }
